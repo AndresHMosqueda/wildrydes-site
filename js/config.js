06613cc71f87ad85bc8e10e99b9707c6654917f5
“AndresHMosqueda“ <andres@gmail.com>
2024-08-10 13:54:15 -0500

    first commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_jiUiqmnVp', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '72ucc3005v9ph8q5568jtgcksu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://eh97x89kbc.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
